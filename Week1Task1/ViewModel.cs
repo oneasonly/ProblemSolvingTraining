a4275153f78d5ef67584a03aefa28327e4329a4f
HomePC <HomePC@DESKTOP-J269GUU>
2021-07-26 09:32:07 +0300

    Add project files.

---
+++ Week1Task1/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Caliburn.Micro;
+
+namespace Week1Task1
+{
+    public class ViewModel : PropertyChangedBase
+    {
+        public uint BanditAmount { get => _banditAmount; set => Set(ref _banditAmount, value); }
+        private uint _banditAmount;
+        public uint EveryN { get => _everyN; set => Set(ref _everyN, value); }
+        private uint _everyN;
+        public uint LastKilledNumber { get => _lastKilledNumber; set => Set(ref _lastKilledNumber, value); }
+        private uint _lastKilledNumber;
+
+        public void Calculate()
+        {
+            var bandits = InitBandits();
+            int index = 0;
+            int killCount = 1;
+            while(bandits.Count > 1)
+            {
+                if (index >= bandits.Count) index = 0;
+                if (killCount == EveryN)
+                {
+                    bandits.RemoveAt(index);
+                    killCount = 1;
+                }
+                if (index >= bandits.Count) index = 0;
+                index++;
+                killCount++;
+            }
+            LastKilledNumber = bandits[0].InitialNumber;
+        }
+
+        private List<Bandit> InitBandits()
+        {
+            var list = new List<Bandit>((int)BanditAmount);
+            for (uint i = 0; i < BanditAmount; i++)
+            {
+                list.Add(new Bandit(i + 1));
+            }
+            return list;
+        }
+        public struct Bandit
+        {
+            public Bandit(uint number) => InitialNumber = number;
+            public uint InitialNumber { get; private set; }
+        }
+    }
+}
---
+++ Week1Task3/ViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace Week1Task3
+{
+    public class ViewModel : PropertyChangedBase
+    {
+        #region props
+        public string Folder { get => _folder; set => Set(ref _folder, value); }
+        private string _folder;
+        public uint Depth { get => _depth; set => Set(ref _depth, value); }
+        private uint _depth;
+        public BindableCollection<string> Content { get; } = new BindableCollection<string>();
+        #endregion
+
+        #region Recursion
+        public async void ShowByRecursion()
+        {
+            if (!Directory.Exists(Folder))
+            {
+                MessageBox.Show($"This folder doesnt exist: {Folder}");
+                return;
+            }
+            Content.Clear();
+            var tempAccumulator = new List<string>();
+            await SelfRecurs(tempAccumulator, Folder, 0);
+
+            tempAccumulator.Sort();
+            Content.AddRange(tempAccumulator);
+        }
+        private async Task SelfRecurs(List<string> accumulator, string incPath, int currDepth)
+        {
+            if (currDepth >= Depth) return;
+            currDepth++;
+            var folders = await Task.Run(() =>
+            {
+                var entries = Directory.GetFileSystemEntries(incPath).Select(x => x.Replace(Folder, "").Trim('\\'));
+                accumulator.AddRange(entries);
+                return Directory.GetDirectories(incPath);
+            });
+            
+            foreach (var folder in folders)
+            {
+                await SelfRecurs(accumulator, folder, currDepth);
+            }
+        }
+        #endregion
+
+        #region Iteration
+        public async void ShowByIteration()
+        {
+            if (!Directory.Exists(Folder))
+            {
+                MessageBox.Show($"This folder doesnt exist: {Folder}");
+                return;
+            }
+            Content.Clear();
+            var tempAccumulator = new List<string>();
+
+            var FoldersByDepth = new List<IEnumerable<string>>((int)Depth);
+            IEnumerable<string> folders = new string[] { Folder };
+            for (int i = 0; i < Depth; i++)
+            {
+                AccumulateEntries(tempAccumulator, folders);
+                FoldersByDepth.Add(await GetFoldersNextLevel(folders));
+                if (FoldersByDepth.Count > 0)
+                {
+                    folders = FoldersByDepth.LastOrDefault();
+                }                                
+            }
+
+            tempAccumulator.Sort();
+            Content.AddRange(tempAccumulator);
+        }
+        private async Task<IList<string>> GetFoldersNextLevel(IEnumerable<string> folders)
+        {
+            return await Task.Run(() =>
+            {
+                var result = new List<string>();
+                foreach (var folder in folders)
+                {
+                    result.AddRange(Directory.GetDirectories(folder));
+                }
+                return result;
+            });
+        }
+        private void AccumulateEntries(List<string> entriesAccumulator, IEnumerable<string> folders)
+        {
+            foreach (var folder in folders)
+            {
+                var entries = Directory.GetFileSystemEntries(folder).Select(x => x.Replace(Folder, "").Trim('\\'));
+                entriesAccumulator.AddRange(entries);
+            }
+        }
+        #endregion
+    }
+}
